83295cb7588eafd65d9c59a8190279aeb3924fd4
Sanhaa <livia3609@gmail.com>
2023-12-09 12:02:13 +0900

    init

---
+++ backend/README.md
@@ -0,0 +1,18 @@
+# booklog-init
+My Demo Project
+
+> 아직 계획중 ~~ 요구사항 정리하다보면 노션이랑 차별점을 찾기 힘들어서 고민이다. 
+
+## Env
+- IntelliJ
+- Spring Boot
+
+- Mybatis
+- Oracle DB (mysql나 maria 쓸지도?)
+- Vue.js (아직 셋팅 안함)
+
+
+## 주요 목적
+- Spring Boot 공부
+- Spring Boot + vue.js(프론트) + mybatis(백) 조합
+
---
+++ backend/src/main/java/sh/project/bookloginit/BooklogInitApplication.java
@@ -0,0 +1,13 @@
+package sh.project.bookloginit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklogInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklogInitApplication.class, args);
+	}
+
+}
