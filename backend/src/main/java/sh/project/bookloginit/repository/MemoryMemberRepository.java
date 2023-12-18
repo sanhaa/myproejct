98902da6a8f63785fc1c79635310b09c951d74b2
Sanhaa <livia3609@gmail.com>
2023-12-18 22:47:55 +0900

    Member Repository 생성 및 Test 코드 작성

---
+++ backend/src/main/java/sh/project/bookloginit/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package sh.project.bookloginit.repository;
+
+import sh.project.bookloginit.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; // 자동 ID생성(key 값)
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member-> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearAll(){
+        store.clear();
+    }
+}
